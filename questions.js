6d540df204f6eb8426412224db7e260222ea2175
chicoben <139389720+chicoben@users.noreply.github.com>
2023-07-12 19:37:58 -0300

    upload reciquiz

---
+++ questions.js
@@ -0,0 +1,102 @@
+export const questions = [
+    {
+      question: "Qual é a definição de reciclagem?",
+
+      answers: [
+        { option: "a)Transformação de resíduos em energia", correct: false },
+        { option: "b)Reutilização de materiais descartados", correct: true },
+        { option: "c)Compostagem de resíduos orgânicos", correct: false },
+        { option: "d)Incineração de resíduos perigosos", correct: false },
+      ],
+    },
+    {
+      question: "Qual dos seguintes materiais é 100% reciclável?",
+
+      answers: [
+        { option: "a)Papel", correct: false },
+        { option: "b)Plástico", correct: false },
+        { option: "c)Vidro", correct: true },
+        { option: "d)Isopor", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o objetivo principal da reciclagem?",
+
+      answers: [
+        { option: "a)Reduzir a quantidade de resíduos enviados para aterros sanitários", correct: true },
+        { option: "b)Aumentar a produção de materiais descartáveis", correct: false },
+        { option: "c)Economizar energia na produção de novos materiais", correct: false },
+        { option: "d)Aumentar a poluição ambiental", correct: false },
+      ],
+    },
+    {
+      question: "O que significa o símbolo de reciclagem composto por três setas em forma de um triângulo fechado?",
+
+      answers: [
+        { option: "a)Material reciclável", correct: true },
+        { option: "b)Material não reciclável", correct: false },
+        { option: "c)Material biodegradável", correct: false },
+        { option: "d)Material perigoso", correct: false },
+      ],
+    },
+  {
+    question: "Qual é o processo de transformar materiais recicláveis em novos produtos chamado?",
+
+    answers: [
+      { option: "a)Reprocessamento", correct: true },
+      { option: "b)Recuperação de energia", correct: false },
+      { option: "c)Reutilização", correct: false },
+      { option: "d)Compostagem", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o material que leva mais tempo para se decompor na natureza?",
+
+    answers: [
+      { option: "a)Papel", correct: false },
+      { option: "b)Plástico", correct: true },
+      { option: "c)Vidro", correct: false },
+      { option: "d)Metal", correct: false },
+    ],
+  },
+  {
+    question: "Qual dos seguintes itens não deve ser reciclado?",
+
+    answers: [
+      { option: "a)Garrafas de vidro", correct: false },
+      { option: "b)Latas de alumínio", correct: false },
+      { option: "c)Papel sujo ou contaminado", correct: true },
+      { option: "d)Embalagens de plástico", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o nome dado ao processo de decomposição natural de resíduos orgânicos?",
+
+    answers: [
+      { option: "a)Compostagem", correct: true },
+      { option: "b)Incineração", correct: false },
+      { option: "c)Aterramento", correct: false },
+      { option: "d)Recuperação de energia", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o material mais comumente reciclado no mundo?",
+
+    answers: [
+      { option: "a)Papel", correct: true },
+      { option: "b)Plásticos", correct: false },
+      { option: "c)Vidro", correct: false },
+      { option: "d)Metal", correct: false },
+    ],
+  },
+  {
+    question: "Quais dos seguintes materiais não são biodegradáveis?",
+ 
+    answers: [
+      { option: "a)Papel e papelão", correct: false },
+      { option: "b)Vidro e metal", correct: false },
+      { option: "c)Plástico e isopor", correct: true },
+      { option: "d)Tecido e madeira", correct: false },
+    ],
+  },
+  ];
